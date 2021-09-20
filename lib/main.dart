64fe5872c3dc290011e7f7b95a6bd82f5c9d7f80
sachin7977 <sachinvishwakarma7977@gmail.com>
2021-09-20 19:54:01 +0530

    Added LightTheme and DarkTheme

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,7 @@
 import 'package:ecommerce/Pages/HomePage.dart';
 import 'package:ecommerce/Pages/LoginPage.dart';
 import 'package:ecommerce/Pages/routes.dart';
+import 'package:ecommerce/widgets/themes.dart';
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
 
@@ -13,12 +14,9 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      themeMode: ThemeMode.dark,
-      theme: ThemeData(
-        primarySwatch: Colors.deepPurple,
-      ),
-      darkTheme: ThemeData(
-          brightness: Brightness.light, primarySwatch: Colors.deepPurple),
+      themeMode: ThemeMode.light,
+      theme: MyTheme.lightTheme(context),
+      darkTheme: MyTheme.darkTheme(context),
       initialRoute: MyRoutes.homeRoute,
       routes: {
         "/": (context) => LoginPage(),
