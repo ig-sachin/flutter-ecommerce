725a7f91b2ebdcf87680d19072d8582b1cbc25b7
sachin7977 <sachinvishwakarma7977@gmail.com>
2021-09-23 12:15:57 +0530

    Added CartPage Content

--- lib/Pages/CartPage.dart
+++ lib/Pages/CartPage.dart
@@ -12,6 +12,65 @@ class CartPage extends StatelessWidget {
         title: "Cart".text.xl3.make(),
       ),
       backgroundColor: Theme.of(context).canvasColor,
+      body: Column(
+        children: [
+          _CartList().p32().expand(),
+          Divider(),
+          _CartTotal(),
+        ],
+      ),
+    );
+  }
+}
+
+class _CartTotal extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return SizedBox(
+      height: 150,
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          "\$9999".text.xl4.make(),
+          30.widthBox,
+          ElevatedButton(
+            onPressed: () {},
+            child: Container(
+              width: 80,
+              height: 50,
+              child: Center(
+                child: Text(
+                  "Buy",
+                  style: TextStyle(
+                    fontSize: 20,
+                  ),
+                ),
+              ),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
+
+class _CartList extends StatelessWidget {
+  const _CartList({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return ListView.builder(
+      itemCount: 20,
+      itemBuilder: (context, index) => ListTile(
+        leading: Icon(Icons.done),
+        trailing: IconButton(
+          onPressed: () {},
+          icon: Icon(
+            Icons.remove_circle_outline,
+          ),
+        ),
+        title: "Item 1".text.make(),
+      ),
     );
   }
 }
